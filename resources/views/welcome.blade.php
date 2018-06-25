438d94083fc47af9656d57ae4a2dcff8ae6da7c1
minako.yanagi <mnk14s@yahoo.co.jp>
2018-06-25 03:45:22 +0000

    register and login function

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,8 @@
     <div class="cover">
         <div class="cover-inner">
             <div class="cover-contents">
-                <h1>素敵なモノと出会う場所</h1>
-                <a href="" class="btn btn-success btn-lg">モノリストを始める</a>
+                <h1>素敵なレシピと出会う場所</h1>
+                <a href="" class="btn btn-success btn-lg">レシピを見てみる</a>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ Route::get('/', 'WelcomeController@index');
 // User registration
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');

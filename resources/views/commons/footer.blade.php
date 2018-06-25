41d331eed259d42eb9309143b6fef205a93cc0e1
minako.yanagi <mnk14s@yahoo.co.jp>
2018-06-25 03:28:22 +0000

    top page

---
+++ resources/views/commons/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+    <div class="text-center text-muted">© 2016 MONOLIST.</div>
+</footer>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Monolist</title>
+
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+        <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
+    </head>
+    <body>
+        @include('commons.navbar')
+
+        @yield('cover')
+
+        <div class="container">
+            @include('commons.error_messages')
+            @yield('content')
+        </div>
+
+        @include('commons.footer')
+    </body>
+</html>

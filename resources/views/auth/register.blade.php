a61c63c54a86e9486fafc8d55e2ba309bba9cd2b
minako.yanagi <mnk14s@yahoo.co.jp>
2018-06-25 03:37:07 +0000

    user registration

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row">
+    <div class="col-xs-offset-3 col-xs-6">
+        <div class="panel panel-default">
+            <div class="panel-heading">会員登録</div>
+            <div class="panel-body">
+                {!! Form::open(['route' => 'signup.post']) !!}
+                    <div class="form-group">
+                        {!! Form::label('name', 'お名前') !!}
+                        {!! Form::text('name', old('name'), ['class' => 'form-control']) !!}
+                    </div>
+
+                    <div class="form-group">
+                        {!! Form::label('email', 'メールアドレス') !!}
+                        {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
+                    </div>
+
+                    <div class="form-group">
+                        {!! Form::label('password', 'パスワード') !!}
+                        {!! Form::password('password', ['class' => 'form-control']) !!}
+                    </div>
+
+                    <div class="form-group">
+                        {!! Form::label('password_confirmation', 'パスワード（確認）') !!}
+                        {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
+                    </div>
+
+                    <div class="text-right">
+                        {!! Form::submit('登録する', ['class' => 'btn btn-success']) !!}
+                    </div>
+                {!! Form::close() !!}
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,7 @@
 */
 
 Route::get('/', 'WelcomeController@index');
+
+// User registration
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');

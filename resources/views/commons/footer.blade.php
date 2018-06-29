61d7e3e952dde0917fadaf0f3e78ebede4765e2d
kohei.shinohara <14kc1026@gmail.com>
2018-06-29 04:52:21 +0000

    footer edited, added GOLDEN FOUR MENTORS

--- resources/views/commons/footer.blade.php
+++ resources/views/commons/footer.blade.php
@@ -1,3 +1,3 @@
 <footer>
-    <div class="text-center text-muted">© 2018 Pocket Recipe.</div>
+    <div class="text-center text-muted">© 2018 POCKET 6 supported by GOLDEN FOUR MENTORS.</div>
 </footer>
